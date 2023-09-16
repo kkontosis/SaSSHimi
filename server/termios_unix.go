b298e2f4dd725122ad726b899e76bc498b2b466e
Kimon Kontosis <kimon@pegasusdev.io>
2023-09-16 15:16:56 +0300

    patch for mac

--- server/termios_unix.go
+++ server/termios_unix.go
@@ -9,10 +9,10 @@ import (
 )
 
 func TermiosSaveStdin() *unix.Termios {
-	termios, _ := unix.IoctlGetTermios(int(syscall.Stdin), unix.TCGETS)
+	termios, _ := unix.IoctlGetTermios(int(syscall.Stdin), unix.TIOCGETA)
 	return termios
 }
 
 func TermiosRestoreStdin(value *unix.Termios) {
-	unix.IoctlSetTermios(int(syscall.Stdin), unix.TCGETS, value)
+	unix.IoctlSetTermios(int(syscall.Stdin), unix.TIOCSETA, value)
 }
